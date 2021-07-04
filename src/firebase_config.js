05dda47f4468fdc59770ba82105877514e18d091
DivyanshuGour <78845422+DivyanshuGour@users.noreply.github.com>
2021-07-04 16:28:08 +0530

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -1,13 +1,6 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCwHyBWULkh77YOvJnwad4qDgOH50NgNUM",
-    authDomain: "react-todo-8d233.firebaseapp.com",
-    projectId: "react-todo-8d233",
-    storageBucket: "react-todo-8d233.appspot.com",
-    messagingSenderId: "227993572883",
-    appId: "1:227993572883:web:a2b06c5266ff1cc0f751e6",
-    measurementId: "G-MSYQHVDFWZ"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
